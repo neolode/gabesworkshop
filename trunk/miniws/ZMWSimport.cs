9f0a5eade64bfb21f30b3fbb691051379a6c2615
neo.lode <neo.lode@a3285058-a43e-11de-b195-ab392a2a73f5>
2009-10-20 13:25:59 +0000

    Added miniws to the repository

---
+++ trunk/miniws/ZMWSimport.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace miniws
+{
+    class ZMWSimport
+    {
+        [DllImport("ZazouMiniWebServer.dll")]
+        public static extern int zmws_easy_start();
+        [DllImport("ZazouMiniWebServer.dll")]
+        public static extern String zmws_get_version();
+        [DllImport("ZazouMiniWebServer.dll")]
+        public static extern void zmws_browse();
+        [DllImport("ZazouMiniWebServer.dll")]
+        public static extern bool zmws_configure();
+        [DllImport("ZazouMiniWebServer.dll")]
+        public static extern int zmws_stop();
+        [DllImport("ZazouMiniWebServer.dll")]
+        public static extern int zmws_get_port();
+    }
+}
