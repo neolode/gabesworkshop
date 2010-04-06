5386dc443010aa7864a6d1561762787940c61db2
neo.lode <neolode@users.noreply.github.com>
2010-04-06 18:56:23 +0000

    Self contained webserver app added

---
+++ miniws/App.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace miniws
+{
+    static class Program
+    {
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            IntPtr hWnd = FindWindow(null, Console.Title);
+            //Hide the window                   
+            ShowWindow(hWnd, 0);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new miniwsForm());
+        }
+    }
+}
