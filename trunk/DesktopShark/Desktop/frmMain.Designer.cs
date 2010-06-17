1f04730acb4224f8be60828b560e6d0364bf44b7
neo.lode <neo.lode@a3285058-a43e-11de-b195-ab392a2a73f5>
2010-06-17 10:55:32 +0000

    First fully working version of DeskShark, my unofficial GrooveShark client

--- trunk/DesktopShark/Desktop/frmMain.Designer.cs
+++ trunk/DesktopShark/Desktop/frmMain.Designer.cs
@@ -30,11 +30,138 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
-            this.wbPlayer = new System.Windows.Forms.WebBrowser();
             this.startBuffer = new System.Windows.Forms.Timer(this.components);
+            this.trayIco = new System.Windows.Forms.NotifyIcon(this.components);
+            this.appLayout = new System.Windows.Forms.SplitContainer();
+            this.pbMin = new System.Windows.Forms.PictureBox();
+            this.pbMax = new System.Windows.Forms.PictureBox();
+            this.pbClose = new System.Windows.Forms.PictureBox();
+            this.pbIco = new System.Windows.Forms.PictureBox();
+            this.lblTitle = new System.Windows.Forms.Label();
             this.bmpSplash = new System.Windows.Forms.PictureBox();
+            this.wbPlayer = new System.Windows.Forms.WebBrowser();
+            this.appLayout.Panel1.SuspendLayout();
+            this.appLayout.Panel2.SuspendLayout();
+            this.appLayout.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMax)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIco)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.bmpSplash)).BeginInit();
             this.SuspendLayout();
+            // 
+            // startBuffer
+            // 
+            this.startBuffer.Interval = 4000;
+            this.startBuffer.Tick += new System.EventHandler(this.Timer1Tick);
+            // 
+            // trayIco
+            // 
+            this.trayIco.Icon = ((System.Drawing.Icon)(resources.GetObject("trayIco.Icon")));
+            this.trayIco.Text = "DeskShark";
+            this.trayIco.DoubleClick += new System.EventHandler(this.pbMin_Click);
+            // 
+            // appLayout
+            // 
+            this.appLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.appLayout.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.appLayout.IsSplitterFixed = true;
+            this.appLayout.Location = new System.Drawing.Point(0, 0);
+            this.appLayout.Name = "appLayout";
+            this.appLayout.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // appLayout.Panel1
+            // 
+            this.appLayout.Panel1.Controls.Add(this.pbMin);
+            this.appLayout.Panel1.Controls.Add(this.pbMax);
+            this.appLayout.Panel1.Controls.Add(this.pbClose);
+            this.appLayout.Panel1.Controls.Add(this.pbIco);
+            this.appLayout.Panel1.Controls.Add(this.lblTitle);
+            this.appLayout.Panel1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.appLayout.Panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.appLayout_Panel1_MouseDown);
+            this.appLayout.Panel1.SizeChanged += new System.EventHandler(this.appLayout_Panel1_SizeChanged);
+            this.appLayout.Panel1MinSize = 32;
+            // 
+            // appLayout.Panel2
+            // 
+            this.appLayout.Panel2.Controls.Add(this.bmpSplash);
+            this.appLayout.Panel2.Controls.Add(this.wbPlayer);
+            this.appLayout.Size = new System.Drawing.Size(784, 584);
+            this.appLayout.SplitterDistance = 34;
+            this.appLayout.SplitterWidth = 1;
+            this.appLayout.TabIndex = 4;
+            this.appLayout.TabStop = false;
+            // 
+            // pbMin
+            // 
+            this.pbMin.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pbMin.Image = global::DeskShark.Properties.Resources.btn_min;
+            this.pbMin.Location = new System.Drawing.Point(688, 0);
+            this.pbMin.Name = "pbMin";
+            this.pbMin.Size = new System.Drawing.Size(32, 34);
+            this.pbMin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbMin.TabIndex = 4;
+            this.pbMin.TabStop = false;
+            this.pbMin.Click += new System.EventHandler(this.pbMin_Click);
+            // 
+            // pbMax
+            // 
+            this.pbMax.BackColor = System.Drawing.Color.Orange;
+            this.pbMax.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pbMax.Image = global::DeskShark.Properties.Resources.btn_max;
+            this.pbMax.Location = new System.Drawing.Point(720, 0);
+            this.pbMax.Name = "pbMax";
+            this.pbMax.Size = new System.Drawing.Size(32, 34);
+            this.pbMax.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbMax.TabIndex = 3;
+            this.pbMax.TabStop = false;
+            this.pbMax.Click += new System.EventHandler(this.pbMax_Click);
+            // 
+            // pbClose
+            // 
+            this.pbClose.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pbClose.Image = global::DeskShark.Properties.Resources.btn_close;
+            this.pbClose.Location = new System.Drawing.Point(752, 0);
+            this.pbClose.Name = "pbClose";
+            this.pbClose.Size = new System.Drawing.Size(32, 34);
+            this.pbClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbClose.TabIndex = 2;
+            this.pbClose.TabStop = false;
+            this.pbClose.Click += new System.EventHandler(this.pbClose_Click);
+            // 
+            // pbIco
+            // 
+            this.pbIco.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pbIco.Image = global::DeskShark.Properties.Resources.gs_ico;
+            this.pbIco.Location = new System.Drawing.Point(0, 0);
+            this.pbIco.Name = "pbIco";
+            this.pbIco.Size = new System.Drawing.Size(46, 34);
+            this.pbIco.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pbIco.TabIndex = 1;
+            this.pbIco.TabStop = false;
+            this.pbIco.MouseDown += new System.Windows.Forms.MouseEventHandler(this.appLayout_Panel1_MouseDown);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(52, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(210, 18);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "DeskShark ~ initializing";
+            this.lblTitle.MouseDown += new System.Windows.Forms.MouseEventHandler(this.appLayout_Panel1_MouseDown);
+            // 
+            // bmpSplash
+            // 
+            this.bmpSplash.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bmpSplash.Image = global::DeskShark.Properties.Resources.gs;
+            this.bmpSplash.Location = new System.Drawing.Point(0, 0);
+            this.bmpSplash.Name = "bmpSplash";
+            this.bmpSplash.Size = new System.Drawing.Size(784, 549);
+            this.bmpSplash.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.bmpSplash.TabIndex = 4;
+            this.bmpSplash.TabStop = false;
             // 
             // wbPlayer
             // 
@@ -43,45 +170,37 @@
             this.wbPlayer.Location = new System.Drawing.Point(0, 0);
             this.wbPlayer.MinimumSize = new System.Drawing.Size(20, 20);
             this.wbPlayer.Name = "wbPlayer";
-            this.wbPlayer.ScriptErrorsSuppressed = true;
             this.wbPlayer.ScrollBarsEnabled = false;
-            this.wbPlayer.Size = new System.Drawing.Size(784, 562);
-            this.wbPlayer.TabIndex = 1;
+            this.wbPlayer.Size = new System.Drawing.Size(784, 549);
+            this.wbPlayer.TabIndex = 5;
             this.wbPlayer.Url = new System.Uri("http://listen.grooveshark.com/", System.UriKind.Absolute);
             this.wbPlayer.Visible = false;
-            this.wbPlayer.ProgressChanged += new System.Windows.Forms.WebBrowserProgressChangedEventHandler(this.WebBrowser1ProgressChanged);
             this.wbPlayer.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.WebBrowser1Navigating);
+            this.wbPlayer.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.wbPlayer_DocumentCompleted);
             this.wbPlayer.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.WebBrowser1Navigated);
             // 
-            // startBuffer
-            // 
-            this.startBuffer.Interval = 4000;
-            this.startBuffer.Tick += new System.EventHandler(this.Timer1Tick);
-            // 
-            // bmpSplash
-            // 
-            this.bmpSplash.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bmpSplash.Image = global::gs.Properties.Resources.gs;
-            this.bmpSplash.Location = new System.Drawing.Point(0, 0);
-            this.bmpSplash.Name = "bmpSplash";
-            this.bmpSplash.Size = new System.Drawing.Size(784, 562);
-            this.bmpSplash.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.bmpSplash.TabIndex = 3;
-            this.bmpSplash.TabStop = false;
-            // 
-            // frmMain
+            // FrmMain
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.Orange;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(784, 562);
-            this.Controls.Add(this.bmpSplash);
-            this.Controls.Add(this.wbPlayer);
+            this.ClientSize = new System.Drawing.Size(784, 584);
+            this.ControlBox = false;
+            this.Controls.Add(this.appLayout);
             this.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FrmMain";
-            this.Text = "GrooveShark Desktop ~ initializing";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Opacity = 0.9;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.appLayout.Panel1.ResumeLayout(false);
+            this.appLayout.Panel1.PerformLayout();
+            this.appLayout.Panel2.ResumeLayout(false);
+            this.appLayout.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMax)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIco)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.bmpSplash)).EndInit();
             this.ResumeLayout(false);
 
@@ -89,9 +208,16 @@
 
         #endregion
 
-        private System.Windows.Forms.WebBrowser wbPlayer;
         private System.Windows.Forms.Timer startBuffer;
+        private System.Windows.Forms.NotifyIcon trayIco;
+        private System.Windows.Forms.SplitContainer appLayout;
         private System.Windows.Forms.PictureBox bmpSplash;
+        private System.Windows.Forms.WebBrowser wbPlayer;
+        private System.Windows.Forms.PictureBox pbIco;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.PictureBox pbMin;
+        private System.Windows.Forms.PictureBox pbMax;
+        private System.Windows.Forms.PictureBox pbClose;
 
     }
 }
